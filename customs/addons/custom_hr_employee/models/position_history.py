96240ab725762610b320dde4c079147304dd0e2d
Kitikawin Hompan <beam222_@hotmail.com>
2025-09-05 11:04:54 +0700

    [FIX] custom_hr_employee: fix position history

--- customs/addons/custom_hr_employee/models/position_history.py
+++ customs/addons/custom_hr_employee/models/position_history.py
@@ -4,19 +4,20 @@ class PositionHistory(models.Model):
     _name = 'hr.position.history'
     _description = 'Position History'
 
-    employee_id = fields.Many2one('hr.employee', string='Employee', readonly=True)
+    employee_id = fields.Many2one('hr.employee', string='Employee', readonly=True, required=True)
     job_id = fields.Many2one('hr.job', string='Position')
-    start_date = fields.Date(string='Start Date')
+    start_date = fields.Date(string='Start Date', required=True)
     department_id = fields.Many2one('hr.department', string='Department')
     end_date = fields.Date(string='End Date')
     description = fields.Text(string='Description')
 
     @api.model_create_multi
-    def create(self, vals):
-        res = super().create(vals)
-        if res.employee_id:
-            res.employee_id.job_id = res.job_id
-            res.employee_id.department_id = res.department_id
-            res.employee_id.position_change_date = res.start_date
-            res.employee_id.end_date = res.end_date 
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        for rec in res:  
+            if rec.employee_id:
+                rec.employee_id.job_id = rec.job_id
+                rec.employee_id.department_id = rec.department_id
+                rec.employee_id.position_change_date = rec.start_date
+                rec.employee_id.end_date = rec.end_date
         return res
